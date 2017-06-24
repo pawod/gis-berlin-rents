afc1a22653e60146589b097e36ec0ee2bdffe166
wodzu <paulwodz@gmail.com>
2017-06-25 00:47:32 +0200

    update results

--- is24crawler/settings.py
+++ is24crawler/settings.py
@@ -5,12 +5,13 @@ BOT_NAME = 'immoscout24bot'
 ITEM_PIPELINES = {'is24crawler.pipelines.CsvWriterPipeline': 300}
 USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
 DOWNLOAD_DELAY = 3
-PAGE_START = 1
+PAGE_START = 132
 PAGE_END = 183
 
 # results
 PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
-CSV_FILE_PATH = os.path.join(os.path.abspath(os.path.join(PROJECT_DIR, os.pardir)), "out/apartments.csv")
+OUT_DIR = os.path.join(os.path.join(os.path.abspath(os.path.join(PROJECT_DIR, os.pardir)), "out"))
+CSV_FILE_PATH = os.path.join(OUT_DIR, "apartments.csv")
 
 # parsing
 NUM_CRITERIA = 3
