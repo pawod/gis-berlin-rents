10c979279f72e39ab630b4bfb357714d9780f006
wodzu <paulwodz@gmail.com>
2017-06-25 12:25:33 +0200

    testing other format for arcmap import

--- tools/csvformater.py
+++ tools/csvformater.py
@@ -11,8 +11,11 @@ df.columns = column_names
 # specified format at task: rooms | price | area | address | year | UTM x-coord | UTM y-coord | price per square meter
 rdf = pd.DataFrame([df.rooms, df.price, df.area, df.address, pd.to_datetime(df.date).dt.year, df.x, df.y,
                     df.price / df.area]).transpose()
+rcolumns = rdf.columns.values
+rcolumns[-1] = "squaremeter price"
+rdf.columns = rcolumns
 
 filePath = os.path.join(is24crawler.settings.OUT_DIR, "apartments-reformatted.csv")
-rdf.to_csv(filePath, sep = '\t', index = False, header = None)
+rdf.to_csv(filePath, sep = '\t')
 
 print("reformatted csv written to: '{}'".format(filePath))
